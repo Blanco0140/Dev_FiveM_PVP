54a25839e641cb6880fad4aacd79b2a96d5a8222
Blanco0140 <blanco.charboni@gmail.com>
2026-03-22 23:32:12 +0100

    Auto backup 22.03.2026 23:32:12,43

--- txData/FiveMBasicServerCFXDefault_B73498.base/resources/[local]/pvp_inv/fxmanifest.lua
+++ txData/FiveMBasicServerCFXDefault_B73498.base/resources/[local]/pvp_inv/fxmanifest.lua
@@ -16,5 +16,8 @@ ui_page 'html/index.html'
 files {
     'html/index.html',
     'html/style.css',
-    'html/script.js'
+    'html/script.js',
+    'html/img/Weapons/*.png',
+    'html/img/Weapons/*.jpg',
+    'html/img/Weapons/*.jpeg'
 }
